5397d4be23636accfcd9be549074eda0e2f8c17f
Herlea Marius <mariusherlea@gmail.com>
2024-07-19 11:00:06 +0300

    RoomList.tsx

---
+++ components/RoomList.tsx
@@ -0,0 +1,45 @@
+"use  client";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { FaStar, FaStarHalf } from "react-icons/fa";
+
+const RoomList = ({ rooms }: { rooms: any }) => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+      {rooms.data.map((room: any) => {
+        const imgURL = `http://127.0.0.1:1337${room.attributes.image.data?.attributes.url}`;
+
+        return (
+          <div key={room.id}>
+            <Link href={`/room/${room.id}`}>
+              <div className="relative h-[300px] w-full overflow-hidden mb-6">
+                <Image
+                  src={imgURL}
+                  fill
+                  priority
+                  alt="room"
+                  className="object-cover"
+                />
+              </div>
+            </Link>
+            <div className="h-[134px] bg-sky-200">
+              <div className="flex items-center justify-between mb-5">
+                <div>Capacity: {room.attributes.capacity} person(s)</div>
+                <div className="flex gap-1">
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStarHalf />
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default RoomList;
